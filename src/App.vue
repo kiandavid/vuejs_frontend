bf0f40b478dd5df514e76f9c14767c3cafbeed2f
kiandavid <101939007+kiandavid@users.noreply.github.com>
2022-06-19 19:35:10 +0200

    Grid layout and router added
    Grid Layout, was verloren gegangen ist durch das Installieren des Routers ist wieder drin. Routing funktioniert im Content-Div

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,90 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="grid-container">
+    <div class="profil">
+      Profil
+    </div>
+    <div class="header">
+      header
+    </div>
+    <div class="navigation">
+      Navigation
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <div class="content">
+      Content
+      <router-view/>
+    </div>
+  </div>
+
+
 </template>
 
 <style>
+body {
+  margin: 0;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
+  margin: 0;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
 }
 
-nav {
-  padding: 30px;
+.grid-container{
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr 6fr;
+  grid-template-rows: 1fr 8fr;
+  /* grid-template-areas: 
+  "header" "header"
+  "nav" "contento"
+  "footer" "footer"; */
+  gap: 10px;
 }
 
-nav a {
+.header{
+  /* grid-area: header; */
+  background-color: red;
+}
+
+.navigation {
+  /* grid-area: navigation; */
+  background-color: blue;
+
+}
+
+.content{
+  /* grid-area: contento; */
+  background-color: green;
+
+}
+
+.profil{
+  /* grid-area: footer; */
+  background-color: yellow;
+
+}
+
+.navigation a {
   font-weight: bold;
   color: #2c3e50;
 }
 
-nav a.router-link-exact-active {
+.navigation a.router-link-exact-active {
   color: #42b983;
 }
 </style>
