3817c01eebad4e275d7180873427c8efc9133e53
kiandavid <101939007+kiandavid@users.noreply.github.com>
2022-07-09 20:49:11 +0200

    Ein-/Ausblenden der Navigationsleisten
    Man kann über v-if und das Route-Objekt (und somit über das path-Attribut) je nach Pfad einstellen, welche Navigationslinks angezeigt werden

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,10 @@
       <div class="navItem">
         <router-link to="/">Home</router-link>
       </div>
-      <!-- <div class="navItem">
+      <div class="navItem" v-if="$route.path=='/'">
         <router-link to="/course">Kurs</router-link>
       </div>
-      <div class="navItem">
+      <!-- <div class="navItem">
         <router-link to="/excercise">Aufgabe</router-link>
       </div> -->
     </div>
